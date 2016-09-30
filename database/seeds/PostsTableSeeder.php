6db24f30b747daf254aa6a9bb00dd62da6dd619b
Hyunwoo Jang <4everwrz@gmail.com>
2016-09-30 12:42:25 +0900

    PostsController 작업 중

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\Post::truncate();
+
+        $users = App\User::all();
+
+        $users->each(function ($user) {
+            $user->posts()->save(
+                factory(App\Post::class)->make()
+            );
+        });
+    }
+}
